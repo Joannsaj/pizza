017152e49e2fa846228d8fa082e262b67bffb8c4
joansaj <joannsaj@gmail.com>
2020-09-06 12:11:35 +0300

    get toppings price

--- js/scripts.js
+++ js/scripts.js
@@ -29,6 +29,57 @@ $(document).ready(function () {
         }
     };
 
+    Pizza.prototype.getToppingPrice = function () {
+        if (this.size === "large") {
+            if (this.topping === "pepperoni") {
+                return 4500;
+            } else if (this.topping === "mushroom") {
+                return 3000;
+            } else if (this.topping === "onions") {
+                return 1500;
+            } else if (this.topping === "sausage") {
+                return 3000;
+            } else if (this.topping === "bacon") {
+                return 4500;
+            } else if (this.topping === "black olives") {
+                return 4500;
+            } else {
+                return 1500;
+            }
+        } else if (this.size === "medium") {
+            if (this.topping === "pepperoni") {
+                return 4000;
+            } else if (this.topping === "mushroom") {
+                return 2800;
+            } else if (this.topping === "onions") {
+                return 1400;
+            } else if (this.topping === "sausage") {
+                return 2800;
+            } else if (this.topping === "bacon") {
+                return 4000;
+            } else if (this.topping === "black olives") {
+                return 4000;
+            } else {
+                return 1400;
+            }
+        } else {
+            if (this.topping === "pepperoni") {
+                return 3700;
+            } else if (this.topping === "mushroom") {
+                return 2500;
+            } else if (this.topping === "onions") {
+                return 1200;
+            } else if (this.topping === "sausage") {
+                return 2500;
+            } else if (this.topping === "bacon") {
+                return 3700;
+            } else if (this.topping === "black olives") {
+                return 3700;
+            } else {
+                return 1200;
+            }
+        }
+    };
 
 
 
