9315e1b02de4cdcada05a503a0d04a6414deff9a
joansaj <joannsaj@gmail.com>
2020-09-06 11:17:37 +0300

    get size price

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,18 @@ $(document).ready(function () {
     }
 
     Pizza.prototype.getPizzaPrice = function () {
-        return this.getCrustPrice() + this.getToppingPrice() + this.getSizePrice();
+        return this.getSizePrice() + this.getToppingPrice() + this.getCrustPrice();
     };
 
+    Pizza.prototype.getSizePrice = function () {
+        if (this.size === "large") {
+            return 28000;
+        } else if (this.size === "medium") {
+            return 20000;
+        } else {
+            return 15000;
+        }
+    };
 
 
 
