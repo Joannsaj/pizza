db873478ce588e38bf052dae44930e5edf569d9b
joansaj <joannsaj@gmail.com>
2020-09-06 16:36:01 +0300

    add total

--- js/scripts.js
+++ js/scripts.js
@@ -10,9 +10,9 @@ $(document).ready(function () {
     };
 
     Pizza.prototype.getSizePrice = function () {
-        if (this.size === "large") {
+        if (this.size == "large") {
             return 28000;
-        } else if (this.size === "medium") {
+        } else if (this.size == "medium") {
             return 20000;
         } else {
             return 15000;
@@ -20,9 +20,9 @@ $(document).ready(function () {
     };
 
     Pizza.prototype.getCrustPrice = function () {
-        if (this.size === "crispy") {
+        if (this.size == "crispy") {
             return 3000;
-        } else if (this.size === "stuffed") {
+        } else if (this.size == "stuffed") {
             return 5000;
         } else {
             return 4000;
@@ -30,75 +30,88 @@ $(document).ready(function () {
     };
 
     Pizza.prototype.getToppingPrice = function () {
-        if (this.size === "large") {
-            if (this.topping === "pepperoni") {
+        if (this.size == "large") {
+            if (this.topping == "pepperoni") {
                 return 4500;
-            } else if (this.topping === "mushroom") {
+            } else if (this.topping == "mushroom") {
                 return 3000;
-            } else if (this.topping === "onions") {
+            } else if (this.topping == "onions") {
                 return 1500;
-            } else if (this.topping === "sausage") {
+            } else if (this.topping == "sausage") {
                 return 3000;
-            } else if (this.topping === "bacon") {
+            } else if (this.topping == "bacon") {
                 return 4500;
-            } else if (this.topping === "black olives") {
+            } else if (this.topping == "black olives") {
                 return 4500;
-            } else if (this.topping === "green peppers") {
+            } else if (this.topping == "green peppers") {
                 return 2500;
             } else {
                 return 1500;
             }
-        } else if (this.size === "medium") {
-            if (this.topping === "pepperoni") {
+        } else if (this.size == "medium") {
+            if (this.topping == "pepperoni") {
                 return 4000;
-            } else if (this.topping === "mushroom") {
+            } else if (this.topping == "mushroom") {
                 return 2800;
-            } else if (this.topping === "onions") {
+            } else if (this.topping == "onions") {
                 return 1400;
-            } else if (this.topping === "sausage") {
+            } else if (this.topping == "sausage") {
                 return 2800;
-            } else if (this.topping === "bacon") {
+            } else if (this.topping == "bacon") {
                 return 4000;
-            } else if (this.topping === "black olives") {
+            } else if (this.topping == "black olives") {
                 return 4000;
-            } else if (this.topping === "green peppers") {
+            } else if (this.topping == "green peppers") {
                 return 3700;
             } else {
                 return 1400;
             }
         } else {
-            if (this.topping === "pepperoni") {
+            if (this.topping == "pepperoni") {
                 return 3700;
-            } else if (this.topping === "mushroom") {
+            } else if (this.topping == "mushroom") {
                 return 2500;
-            } else if (this.topping === "onions") {
+            } else if (this.topping == "onions") {
                 return 1200;
-            } else if (this.topping === "sausage") {
+            } else if (this.topping == "sausage") {
                 return 2500;
-            } else if (this.topping === "bacon") {
+            } else if (this.topping == "bacon") {
                 return 3700;
-            } else if (this.topping === "black olives") {
+            } else if (this.topping == "black olives") {
                 return 3700;
-            } else if (this.topping === "green peppers") {
+            } else if (this.topping == "green peppers") {
                 return 3700;
             } else {
                 return 1200;
             }
         }
     };
+    var totalCost = 0;
     var pizzasOrdered = [];
-    $("#pizza-form").submit(function (e) {
-        e.preventDefault();
-        var sizeSelected = $("#size").val();
-        var toppingSelected = $("#topping").val();
-        var crustSelected = $("#crust").val();
+    $("#pizza-form").submit(function (event) {
+        event.preventDefault();
+        var sizeSelected = $(this).find("#size").val();
+        var toppingSelected = $(this).find("#topping").val();
+        var crustSelected = $(this).find("#crust").val();
 
         var newPizza = new Pizza(sizeSelected, toppingSelected, crustSelected);
-        pizzasOrdered.push(newPizza)
-        console.log(pizzasOrdered);
+        pizzasOrdered.push(newPizza);
 
+        $("#size").val("");
+        $("#topping").val("");
+        $("#crust").val("");
+
+        totalCost = 0;
+
+        for (let i = 0; i < pizzasOrdered.length; i++) {
+            totalCost += pizzasOrdered[i].getPizzaPrice();
+        }
+        console.log(totalCost);
+        alert("Your order for a" + " " + newPizza.size + " " + "pizza with" + " " + newPizza.topping + " " + "toppings and a" + newPizza.crust + " " + "crust, has been received. Your bill is UGX" + " " + totalCost);
     });
-
+    $("#continue").click(function () {
+        $(".hide").show();
+    });
 
 
 
